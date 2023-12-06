0e1afc7545aa91b2cf33c4cd9225400b1b2d27c3
Janek Vačkář <jackie4u@seznam.cz>
2023-12-06 18:01:28 +0100

    Inital commit of current excercises

---
+++ DuplicatesCounter/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Count the number of Duplicates
+//Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string.
+//The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+//Example
+//"abcde" -> 0 # no characters repeats more than once
+//"aabbcde" -> 2 # 'a' and 'b'
+//"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+//"indivisibility" -> 1 # 'i' occurs six times
+//"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+//"aA11" -> 2 # 'a' and '1'
+//"ABBA" -> 2 # 'A' and 'B' each occur twice
+
+using System;
+using System.Linq;
+
+var tst = "aabbcde";
+
+var o = Kata.DuplicateCount(tst);
+
+Console.WriteLine(o.ToString());
+
+public static class Kata {
+
+    public static int DuplicateCount(string str) {
+        //var input = str.ToLower();
+        //var count = input.GroupBy(s => s)
+        //    .Select(group => new
+        //    {
+        //        key = group.Key,
+        //        cnt = group.Count()
+        //    }).Count(g => g.cnt > 1);
+
+        int result = str.ToLower().GroupBy(x => x).Count(x => x.Count() > 1);
+
+        return result;
+    }
+}
---
+++ PhoneNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Write a function that accepts an array of 10 integers (between 0 and 9), 
+ * that returns a string of those numbers in the form of a phone number.
+ * Example
+ *  Kata.CreatePhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}) // => returns "(123) 456-7890"
+ *  The returned format must be correct in order to complete this challenge.
+ * Don't forget the space after the closing parentheses!
+ */
+
+using System.Text;
+
+namespace PhoneNumber {
+
+    public class Program {
+        static void Main(string[] args) {
+            var numbers = new int[] { 0, 2, 3, 4, 5, 6, 7, 8, 9, 1 };
+            var phone = CreatePhoneNumber(numbers);
+            Console.WriteLine($"{phone}");
+        }
+
+        public static string CreatePhoneNumber(int[] numbers) {
+            // Best answer: 
+            // long.Parse(string.Concat(numbers)).ToString("(000) 000-0000");
+            var a = string.Concat(numbers);
+            var b = long.Parse(a);
+            var c = b.ToString("(000) 000-0000");
+            // / Best answer
+
+            var numbersBuilder = new StringBuilder();
+            foreach (int number in numbers) {
+                numbersBuilder.Append(number);
+            }
+
+            var telephone = numbersBuilder.ToString();
+            if (telephone.Length == 10) {
+                // "(123) 456-7890"
+                telephone = string.Format("({0}) {1}-{2}",
+                    telephone.Substring(0, 3),
+                    telephone.Substring(3, 3),
+                    telephone.Substring(6));
+            }
+            return telephone;
+        }
+
+    }
+}
