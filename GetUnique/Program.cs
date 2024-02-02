fbfd9cc41fdcabf20fdb89041d1315c3402225d6
jackie4u <jackie4u@seznam.cz>
2024-02-02 15:58:14 +0100

    Add GetUnique project and implementation

---
+++ GetUnique/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace GetUnique {
+    internal class Program {
+
+        static void Main(string[] args) {
+            var input = new int[] { 1, 1, 1, 2, 1, 1 };
+            var unique = GetUnique(input);
+            Console.WriteLine($"Unique number: {unique}");
+        }
+
+        static int GetUnique(IEnumerable<int> numbers) => numbers.GroupBy(n => n)
+                .Where(g => g.Count() == 1)
+                .Select(g => g.Key)
+                .FirstOrDefault();
+    }
+}
